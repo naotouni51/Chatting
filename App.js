5d0f1a3589b6cb8a23ca5e18cf91697b3ac3e5a0
naotouni51 <naotouni51@gmail.com>
2019-12-18 13:57:50 +0900

    textinputとscrollview

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, KeyboardAvoidingView, TextInput, ScrollView } from 'react-native';
 import firebase from "firebase";
 require("firebase/firestore")
 
@@ -26,11 +26,14 @@ class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        {this.state.commentList.map((value, index) => {
-          return (
-            <Text key={index}>{value}</Text>
-          )
-        })}
+        <ScrollView style={{flex: 1, backgroundColor: 'red'}}>
+          {this.state.commentList.map((value, index) => {
+            return (
+              <Text key={index}>{value}</Text>
+            )
+          })}
+        </ScrollView>
+        <TextInput style={{backgroundColor: 'white'}} />
       </View>
     );
   }
